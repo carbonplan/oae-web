ab1db2b41b262a975eb227cf72a844d5f06471d1
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-01-11 10:59:42 -0600

    hover graph experimentation

--- components/timeseries-overview.js
+++ components/timeseries-overview.js
@@ -14,19 +14,37 @@ function TimeseriesOverview({ sx }) {
   return (
     <>
       <Box sx={sx.heading}>efficiency</Box>
+      <Box sx={sx.label}>Download CSV</Box>
       <Box sx={{ width: '100%', height: '400px' }}>
         <Chart x={[1999, 2014]} y={[0, 1]} padding={{ left: 60, top: 50 }}>
           <Grid vertical horizontal />
+
           <Ticks left bottom />
           <TickLabels left bottom />
           <AxisLabel left>OAE efficiency</AxisLabel>
           <AxisLabel bottom>Time</AxisLabel>
           <Plot>
             <Line
+              sx={{
+                stroke: 'blue',
+                '&:hover': { stroke: 'red', cursor: 'pointer' },
+              }}
               data={[
                 [2014, 0.9],
                 [2004, 0.75],
                 [2002, 0.5],
+                [1999, 0],
+              ]}
+            />
+            <Line
+              sx={{
+                stroke: 'blue',
+                '&:hover': { stroke: 'red', cursor: 'pointer' },
+              }}
+              data={[
+                [2014, 0.93],
+                [2004, 0.72],
+                [2002, 0.45],
                 [1999, 0],
               ]}
             />
