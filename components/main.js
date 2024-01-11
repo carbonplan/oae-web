bf9c1e3c40b1389aa0f1aab5da85381fc47307ae
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-01-11 09:59:11 -0600

    filter/timeseries shown

--- components/main.js
+++ components/main.js
@@ -4,6 +4,28 @@ import { Box, Divider } from 'theme-ui'
 import Header from './header'
 import MapWrapper from './map'
 import RegionFooter from './footer'
+import Filters from './filters'
+import TimeseriesOverview from './timeseries-overview'
+const sx = {
+  heading: {
+    fontFamily: 'heading',
+    letterSpacing: 'smallcaps',
+    textTransform: 'uppercase',
+    fontSize: [2, 2, 2, 3],
+    mb: [2],
+  },
+  description: {
+    fontSize: [1, 1, 1, 2],
+  },
+  label: {
+    color: 'secondary',
+    fontFamily: 'mono',
+    letterSpacing: 'mono',
+    fontSize: [1, 1, 1, 2],
+    textTransform: 'uppercase',
+    my: 2,
+  },
+}
 
 const Main = () => {
   const [expanded, setExpanded] = useState(true)
@@ -35,8 +57,11 @@ const Main = () => {
               explainer article for more details about the model.
             </Box>
             <Box sx={{ fontSize: 2 }}>Created in collaboration with</Box>
-            <Box sx={{ fontSize: 4, mb: 4 }}>[C]Worthy</Box>
-            <Divider />
+            <Box sx={{ fontSize: 4 }}>[C]Worthy</Box>
+            <Divider sx={{ mt: 4, mb: 5 }} />
+            <Filters sx={sx} />
+            <Divider sx={{ mt: 4, mb: 5 }} />
+            <TimeseriesOverview sx={sx} />
           </Sidebar>
         </MapWrapper>
       </Box>
