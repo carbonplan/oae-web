7e0b322458e147f387d3a15b886803ac2ea75282
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-01-26 16:13:04 -0600

    clean up filters

--- components/filters.js
+++ components/filters.js
@@ -12,34 +12,45 @@ const Filters = ({
   return (
     <>
       <Box sx={sx.heading}>injection</Box>
-      <Row columns={[6, 8, 3, 3]}>
+      <Row columns={[6, 8, 4, 4]}>
         <Column start={1} width={[1]} sx={sx.label}>
           Season
         </Column>
-        <Column start={[2]} width={[2]} sx={sx.label}>
+        <Column start={[3]} width={[2]} sx={sx.label}>
           <Filter
             values={injectionSeason}
             setValues={(val) => setInjectionSeason(val)}
           />
         </Column>
       </Row>
-      <Row columns={[6, 8, 3, 3]}>
-        <Column start={1} width={[1]} sx={sx.label}>
+      <Row columns={[6, 8, 4, 4]}>
+        <Column start={1} width={[2]} sx={sx.label}>
           Time horizon
         </Column>
-        <Column start={[2]} width={[2]} sx={sx.label}>
+        <Column start={[3]} width={[2]} sx={sx.label}>
           <Input
-            sx={{ ...sx.label, width: '50px', my: 0, pt: 0 }}
+            sx={{
+              ...sx.label,
+              width: '35px',
+              my: 0,
+              pt: 0,
+              color: 'primary',
+              display: 'inline',
+            }}
             value={timeHorizon}
             type='number'
             min={0}
             max={15}
             onChange={(e) => {
               const value = parseInt(e.target.value, 10)
-              setTimeHorizon(value > 15 ? 15 : value)
+              if (!isNaN(value)) {
+                setTimeHorizon(value > 15 ? 15 : value)
+              }
             }}
           />
-          years
+          <Box as={'span'} sx={{ ml: 1 }}>
+            years
+          </Box>
         </Column>
       </Row>
     </>
