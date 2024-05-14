fe62763640992efa8e04bd64d06337f43cc1b637
katamartin <katasm@gmail.com>
2024-05-14 17:51:48 -0400

    Update intro

--- components/intro.js
+++ components/intro.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Box } from 'theme-ui'
+import { Link } from '@carbonplan/components'
 
 const Intro = () => {
   return (
     <Box sx={{ fontSize: 1, mb: 3 }}>
       This is an interactive tool for exploring the efficiency of enhanced
-      alkalinity enhancement (OAE). Read the paper or our explainer article for
-      more details about the model.
+      alkalinity enhancement (OAE). Explore global patterns, or drill down to
+      see how alkalinity released in a specific region and season will move
+      through the ocean and result in carbon removal over time. Read the{' '}
+      <Link href='https://doi.org/10.21203/rs.3.rs-4124909/v1'>preprint</Link>{' '}
+      or our <Link href='TK'>explainer article</Link> for more details.
+      Developed in collaboration with{' '}
+      <Link href='https://cworthy.org/'>[C]Worthy</Link>.
     </Box>
   )
 }
