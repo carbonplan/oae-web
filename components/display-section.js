01527c723c995e7cc3dc98683453bb67e724ebe0
katamartin <katasm@gmail.com>
2024-05-14 11:12:43 -0400

    Improve disabled state

--- components/display-section.js
+++ components/display-section.js
@@ -73,7 +73,7 @@ const DisplaySection = ({ sx }) => {
           Variable
         </Column>
         <Column start={[3, 3, 2, 2]} width={[4, 6, 3, 3]}>
-          <Box>
+          <Box sx={{ position: 'relative' }}>
             <Select
               disabled={!selectedRegion}
               onChange={handleFamilySelection}
@@ -83,14 +83,18 @@ const DisplaySection = ({ sx }) => {
                 width: '100%',
                 mr: 2,
                 mb: 1,
-                '&:hover + #description': selectedRegion
+                '&:hover ~ #description': selectedRegion
+                  ? {}
+                  : {
+                      color: 'primary',
+                    },
+                '&:hover ~ #lock-container': selectedRegion
                   ? {}
                   : {
                       color: 'primary',
                     },
               }}
               sxSelect={{
-                cursor: selectedRegion ? 'pointer' : 'help',
                 fontFamily: 'mono',
                 width: '100%',
               }}
@@ -114,6 +118,32 @@ const DisplaySection = ({ sx }) => {
                   selectedRegion ? 'region' : 'overview'
                 ]
               }
+            </Box>
+            <Box
+              id='lock-container'
+              sx={{
+                position: 'absolute',
+                right: 0,
+                top: '2px',
+                background: 'background',
+                pointerEvents: 'none',
+                color: 'secondary',
+                transition: 'all 0.2s',
+              }}
+            >
+              <Box
+                as='svg'
+                viewBox='0 0 24 24'
+                fill='currentColor'
+                xmlns='http://www.w3.org/2000/svg'
+                sx={{ width: [14, 14, 14, 14], height: [14, 14, 14, 16] }}
+              >
+                <path
+                  fill-rule='evenodd'
+                  clip-rule='evenodd'
+                  d='M10.5 1C8.01472 1 6 3.01472 6 5.5V8H4V21C4 22.1046 4.89543 23 6 23H18C19.1046 23 20 22.1046 20 21V8H18V5.5C18 3.01472 15.9853 1 13.5 1H10.5ZM16 8V5.5C16 4.11929 14.8807 3 13.5 3H10.5C9.11929 3 8 4.11929 8 5.5V8H16Z'
+                />
+              </Box>
             </Box>
           </Box>
 
