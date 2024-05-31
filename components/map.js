9da4c3a25c6cad190cf712a87585dace6d639ec7
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-05-31 09:35:13 -0500

    simplify log calc

--- components/map.js
+++ components/map.js
@@ -38,12 +38,7 @@ const frag = `
 
     float rescaled;
     if (useLogScale) {
-      float logMin = log(clim.x)/log(10.0);
-      float logMax = log(clim.y)/log(10.0);
-      float logValue = log(value)/log(10.0);
-      if (logValue < logMin) logValue = logMin;
-      if (logValue > logMax) logValue = logMax;
-      rescaled = (logValue - logMin) / (logMax - logMin);
+      rescaled = (log(value)/log(10.0) - log(clim.x)/log(10.0)) / (log(clim.y)/log(10.0) - log(clim.x)/log(10.0));
     } else {
       rescaled = (value - clim.x) / (clim.y - clim.x);
     }
