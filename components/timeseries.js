cf1d97e2f05728bcfff7cf1f2ba3ec596ce6a3ef
Shane Loeffler <Shane98c@users.noreply.github.com>
2024-06-21 08:48:16 -0500

    one point at a time

--- components/timeseries.js
+++ components/timeseries.js
@@ -327,14 +327,13 @@ const Timeseries = ({
                 color='primary'
                 style={{ strokeDasharray: '2 4' }}
               />
-              {xSelector &&
-                isHovering &&
-                renderPoint({
-                  x: mousePosition,
-                  y: xSelectorValue,
-                  color: 'secondary',
-                })}
-              {point && renderPoint(point)}
+              {xSelector && isHovering
+                ? renderPoint({
+                    x: mousePosition,
+                    y: xSelectorValue,
+                    color: 'secondary',
+                  })
+                : point && renderPoint(point)}
             </>
           )}
         </Plot>
