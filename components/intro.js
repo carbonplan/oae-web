07abf1ce999a3cc464dc0f1df646360c1af67b90
katamartin <katasm@gmail.com>
2024-07-15 12:56:33 -0400

    Update explainer link

--- components/intro.js
+++ components/intro.js
@@ -11,8 +11,9 @@ const Intro = ({ sx }) => {
       month will move through the ocean and result in carbon removal over time.
       Read the{' '}
       <Link href='https://doi.org/10.21203/rs.3.rs-4124909/v1'>preprint</Link>{' '}
-      or our <Link href='TK'>explainer article</Link> for more details.
-      Developed in collaboration with{' '}
+      or our{' '}
+      <Link href='/research/oae-efficiency-explainer'>explainer article</Link>{' '}
+      for more details. Developed in collaboration with{' '}
       <Link href='https://cworthy.org/'>[C]Worthy</Link>.
     </Box>
   )
